029662e94a8ed6840ce10d7361b69b8ec9cd1986
1f10230039 <s1f102300391@iniad.org>
2025-08-21 09:05:04 +0900

    githubpagesのための調整

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-pomodoro-timer/",
+  base: "/pomodoro-timer-react/",
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
